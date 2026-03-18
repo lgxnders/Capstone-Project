e92517fbc7662289dc26a76dddfb1141b7fc5f1a
lgxnders <alexander.stasyna@gmail.com>
2026-03-17 20:56:22 -0400

    Cleaned out old files from directory.

--- server/controllers/chatController.js
+++
@@ -1,8 +0,0 @@
-import { generateResponse } from '../services/chatbot.js';
-
-export const handleChat = async (req, res) => {
-    const { message } = req.body;
-    if (!message) return res.status(400).json({ error: 'Message is required' });
-    const reply = await generateResponse(message);
-    res.json({ reply });
-};
