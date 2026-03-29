f667de7d0e62bdaa2031b0b3a321c35b1f9b4ced
lgxnders <alexander.stasyna@gmail.com>
2026-03-29 16:33:54 -0400

    Removed old codebase from repo.

--- archived-django-repo/backend/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-	path("", lambda r: JsonResponse({"status": "ok"})), # debug
-    path('admin/', admin.site.urls),
-    path('api/', include('apps.api.urls')),
-]
