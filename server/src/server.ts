7417378adcb316bc10fbf7ef8f85cf6674f354f2
lgxnders <alexander.stasyna@gmail.com>
2026-03-29 16:58:14 -0400

    Updated error handling + comments to be more readable.

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,18 +8,28 @@ import chatRoutes from './routes/chat';
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
 
-const app = express();
-const PORT = process.env.PORT;
+// Define the port and check to ensure that it is properly read from .env.
+let PORT = 0;
+try {
+    const PORT = process.env.PORT;
+    if (PORT === undefined) throw new Error("Port is undefined.")
+} catch (err) {
+    console.log('There was an issue assigning the PORT constant a value during server setup.')
+    console.log('Please ensure that the .env file in /server/ is present and contains up-to-date information.')
+    process.exit(1)
+}
 
+// Set up our Express app.
+const app = express();
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
-// defined routes
+// Define routing.
 app.use('/api/chat', chatRoutes);
 app.use('/api/users', userRoutes);
 app.use('/auth', authRoutes);
 
-// connect to database then listen for connection
+// Attempt to connect to database and then listen for requests.
 connectToDB().then( () => {
     app.listen(PORT, () => {
         console.log(`Server running at http://localhost:${PORT}`);
